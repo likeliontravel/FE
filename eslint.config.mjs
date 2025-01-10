204644707a48365bb2c53e4086d38a603f000a8a
jwj9127 <jwj9127@naver.com>
2025-01-10 15:35:42 +0900

    docs(global): 프로젝트 시작 - 테스트입니다.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,9 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat();
 
 export default [
   ...compat.extends(
-    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
