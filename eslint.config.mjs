b018e6bf7530cbc48cd728206c3208e66723abfe
jwj9127 <jwj9127@naver.com>
2025-01-16 17:43:57 +0900

    docs: ESLint 설정 완료 - ESLint 플러그인 추가 - ESLint 규칙 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,13 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
 import prettier from 'eslint-plugin-prettier';
 import tsParser from '@typescript-eslint/parser';
 
 const compat = new FlatCompat();
 
 export default [
-  js.configs.recommended,
   ...compat.extends(
-    'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/strict',
+    'plugin:react-perf/recommended',
     'plugin:prettier/recommended'
   ),
   {
@@ -25,7 +23,7 @@ export default [
     rules: {
       'prettier/prettier': ['error', { endOfLine: 'lf' }],
       semi: ['error', 'always'],
-      quotes: ['error', 'double'],
+      quotes: ['error', 'single'],
       indent: ['error', 2],
       'no-unused-vars': ['warn'],
       'no-console': ['warn'],
