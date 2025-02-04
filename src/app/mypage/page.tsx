4fc80425f72256a7b909a28389d5da1c75e0637a
jwj9127 <jwj9127@naver.com>
2025-02-04 23:17:07 +0900

    feat: mypage div 구성
    - 기능도 퍼블리싱도 없이 div로만 구성해놨습니다

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,22 @@
+export default function myPage() {
+  return (
+    <>
+      {/* 상단 메뉴 */}
+      <div>
+        {/* 회원, 미니 캘린더 */}
+        <div>
+          {/* 회원 */}
+          <div></div>
+          {/* 캘린더 */}
+          <div></div>
+        </div>
+
+        {/* 여행 계획 */}
+        <div></div>
+      </div>
+
+      {/* 프로모션 배너 */}
+      <div></div>
+    </>
+  );
+}
