30e01ff9592ddf4602a2206e3f033e86282de2ce
jwj9127 <jwj9127@naver.com>
2025-03-11 18:26:24 +0900

    feat: 컴포넌트 추가 - ifGroup 컴포넌트를 추가하여 페이지 컴포넌트 정리

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -1,117 +1,20 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import style from '../../../styles/group/groupPage.module.scss';
-import useBetweenScroll from '../../../util/useBetweenScroll';
 import NonGroup from './nonGroup';
+import IfGroup from './ifGroup';
 
-export default function groupPage() {
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-
+const GroupPage = () => {
   const [hotPlaceList, setHotPlaceList] = useState<any>(null);
 
   setHotPlaceList(null);
 
-  useBetweenScroll(scrollContainerRef);
-
   return (
     <div className={style.body}>
-      {/* 그룹 */}
-      <div className={style.top_div}>
-        <div className={style.group_div}>
-          <p>나의 그룹</p>
-          <div className={style.group_craete_div}>
-            <p>그룹 생성</p>
-            <div className={style.group_img}></div>
-          </div>
-        </div>
-        {/* 그룹 리스트 */}
-        {hotPlaceList ? (
-          <div className={style.group_list} ref={scrollContainerRef}>
-            <div className={style.group_content}>
-              <div className={style.group_content_title}>
-                <p>멋사</p>
-              </div>
-              <div className={style.group_content_people}>
-                <div className={style.group_img}></div>
-                <p>8명</p>
-              </div>
-              <div className={style.shapes_img}></div>
-            </div>
-            <div className={style.group_content}>
-              <div className={style.group_content_title}>
-                <p>멋사</p>
-              </div>
-              <div className={style.group_content_people}>
-                <div className={style.group_img}></div>
-                <p>8명</p>
-              </div>
-              <div className={style.shapes_img}></div>
-            </div>
-            <div className={style.group_content}>
-              <div className={style.group_content_title}>
-                <p>멋사</p>
-              </div>
-              <div className={style.group_content_people}>
-                <div className={style.group_img}></div>
-                <p>8명</p>
-              </div>
-              <div className={style.shapes_img}></div>
-            </div>
-            <div className={style.group_content}>
-              <div className={style.group_content_title}>
-                <p>멋사</p>
-              </div>
-              <div className={style.group_content_people}>
-                <div className={style.group_img}></div>
-                <p>8명</p>
-              </div>
-              <div className={style.shapes_img}></div>
-            </div>
-            <div className={style.group_content}>
-              <div className={style.group_content_title}>
-                <p>멋사</p>
-              </div>
-              <div className={style.group_content_people}>
-                <div className={style.group_img}></div>
-                <p>8명</p>
-              </div>
-              <div className={style.shapes_img}></div>
-            </div>
-          </div>
-        ) : (
-          <NonGroup />
-        )}
-      </div>
-      {/* 일정 */}
-      <div className={style.schedule_div}>
-        <div className={style.comming_div}>
-          <div className={style.commig_div_p_div}>
-            <div className={style.commig_div_p_div_flex_div}>
-              <div className={style.commig_div_p_div_flex_div_title}>
-                <p>멋사</p>
-              </div>
-              <p>의</p>
-            </div>
-            <div className={style.commig_div_p_div_flex_div_p}>
-              <p>
-                일정이 <br /> 다가오고있어요
-              </p>
-              <div className={style.ellipse}></div>
-            </div>
-          </div>
-          <div className={style.commig_div_show}>
-            <p>보러가기 {'>'}</p>
-          </div>
-        </div>
-        {/* 그룹 메인 이미지 */}
-        <div>
-          <div className={style.schedule_div_group_img}></div>
-          <div className={style.schedule_div_deadline}>
-            <p>D-12</p>
-          </div>
-        </div>
-      </div>
+      {hotPlaceList ? <IfGroup /> : <NonGroup />}
     </div>
   );
-}
+};
+
+export default GroupPage;
