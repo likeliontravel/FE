a8f0f271390d26b2a7fff24f70db14d221bdaa1d
jwj9127 <jwj9127@naver.com>
2025-11-20 18:04:06 +0900

    feat: store 추가

--- store/store.tsx
+++ store/store.tsx
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../util/login/authSlice';
-import boardReducer from '../util/board/boardSilce';
-import calendarReducer from './calendarSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import groupReducer from "../util/group/groupSlice";
+import authReducer from "../util/login/authSlice";
+import boardReducer from "../util/board/boardSilce";
+import calendarReducer from "./calendarSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       auth: authReducer,
+      group: groupReducer,
       board: boardReducer,
       calendar: calendarReducer,
     },
@@ -14,5 +16,5 @@ export const makeStore = () => {
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
