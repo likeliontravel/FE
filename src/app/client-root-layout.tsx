2246b81e6e96bbac524a55b5ae05e051c9e9b219
jwj9127 <jwj9127@naver.com>
2026-02-05 17:53:45 +0900

    style: 기본 css 수정

--- src/app/client-root-layout.tsx
+++ src/app/client-root-layout.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 import ReduxProvider from "../../store/ReduxProvider";
 import AuthSessionHandler from "./_component/AuthSessionHandler";
 import Header from "../app/_component/Header";
 export default function RootLayout({
-children,
+  children,
 }: {
-children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-return (
-<html lang="ko">
-<body>
-<ReduxProvider>
-<AuthSessionHandler>
-<Header />
-<main>{children}</main>
-</AuthSessionHandler>
-</ReduxProvider>
-</body>
-</html>
-);
+  return (
+    <html lang="ko">
+      <body style={{ margin: 0 }}>
+        <ReduxProvider>
+          <AuthSessionHandler>
+            <Header />
+            <main>{children}</main>
+          </AuthSessionHandler>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
 }
