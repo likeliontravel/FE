c1214a41791f22b8d31b2e7b4b594bc0933041ca
jwj9127 <jwj9127@naver.com>
2026-02-11 16:44:56 +0900

    feat: chatReducer 추가

--- store/store.tsx
+++ store/store.tsx
@@ -4,12 +4,14 @@ import authReducer from "../util/login/authSlice";
 import boardReducer from "../util/board/boardSilce";
 import scheduleReducer from "../util/schedule/scheduleSilce";
 import calendarReducer from "./calendarSlice";
+import chatReducer from "../util/group/chat/chatSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       auth: authReducer,
       group: groupReducer,
+      chat: chatReducer,
       board: boardReducer,
       calendar: calendarReducer,
       schedule: scheduleReducer,
