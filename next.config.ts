46b86dc29faf7650c8385f031bb90ab54105111a
jwj9127 <jwj9127@naver.com>
2026-02-11 15:44:00 +0900

    fix: api url 수정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/',
-        destination: '/main',
+        source: "/",
+        destination: "/main",
       },
     ];
   },
@@ -13,40 +13,46 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*', // 모든 API 경로에 대해 CORS 헤더를 적용
+        source: "/api/:path*", // 모든 API 경로에 대해 CORS 헤더를 적용
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://toleave.shop' }, // 프론트 도메인
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
+            key: "Access-Control-Allow-Origin",
+            value: "https://toleave.cloud",
+          }, // 프론트 도메인
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
           {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Authorization, Refresh-Token, Content-Type',
+            key: "Access-Control-Allow-Headers",
+            value: "Authorization, Refresh-Token, Content-Type",
           },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
-            key: 'Access-Control-Expose-Headers',
-            value: 'Authorization, Refresh-Token',
+            key: "Access-Control-Expose-Headers",
+            value: "Authorization, Refresh-Token",
           },
         ],
       },
       {
-        source: '/:path*', // 모든 경로에 대해서도 CORS를 적용
+        source: "/:path*", // 모든 경로에 대해서도 CORS를 적용
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://toleave.shop' }, // 프론트 도메인
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
+            key: "Access-Control-Allow-Origin",
+            value: "https://toleave.cloud",
+          }, // 프론트 도메인
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
           {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Authorization, Refresh-Token, Content-Type',
+            key: "Access-Control-Allow-Headers",
+            value: "Authorization, Refresh-Token, Content-Type",
           },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
-            key: 'Access-Control-Expose-Headers',
-            value: 'Authorization, Refresh-Token',
+            key: "Access-Control-Expose-Headers",
+            value: "Authorization, Refresh-Token",
           },
         ],
       },
