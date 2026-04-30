ef09bcea949cba8a0cd9835488083b0be818b71f
jwj9127 <jwj9127@naver.com>
2026-04-30 15:39:58 +0900

    feat: 일정 목록 조회

--- util/select/UseReactSelect.tsx
+++ util/select/UseReactSelect.tsx
@@ -1,7 +1,7 @@
 // components/select/UseReactSelect.tsx
 "use client";
 
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import Select, { SingleValue, ActionMeta } from "react-select";
 import CalendarOption from "./CalendarOption";
 import ListOption from "./ListOption";
@@ -11,11 +11,13 @@ import "./Select.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../../store/store";
 import {
-  setSelectedCalendarSchedule, 
+  setSelectedCalendarSchedule,
   setSelectedListSchedule,
   ScheduleOption,
 } from "../../store/calendarSlice";
 import style from "./Select.module.scss";
+import dayjs from "dayjs";
+import { api } from "../api";
 
 interface UseReactSelectProps {
   type: "calendar" | "list";
@@ -24,30 +26,56 @@ interface UseReactSelectProps {
 const UseReactSelect = ({ type }: UseReactSelectProps) => {
   const dispatch = useDispatch<AppDispatch>();
   const selectedCalendarSchedule = useSelector(
-    (s: RootState) => s.calendar.selectedCalendarSchedule
+    (s: RootState) => s.calendar.selectedCalendarSchedule,
   );
   const selectedListSchedule = useSelector(
-    (s: RootState) => s.calendar.selectedListSchedule
+    (s: RootState) => s.calendar.selectedListSchedule,
   );
 
-  const calendarOptions = useMemo<ScheduleOption[]>(
-    () => [
-      { value: "default", label: "-", prefix: "내일정", suffix: "D-" },
-      {
-        value: "trip_sokcho",
-        label: "속초 여행",
-        prefix: "멋사",
-        suffix: "D-21",
-      },
-      {
-        value: "trip_jeju",
-        label: "제주도 여행",
-        prefix: "가나다라",
-        suffix: "D-21",
-      },
-    ],
-    []
-  );
+  const [fetchedCalendarOptions, setFetchedCalendarOptions] = useState<
+    ScheduleOption[]
+  >([
+    { value: "default", label: "-", prefix: "내일정", suffix: "D-" }, // 기본값
+  ]);
+
+  useEffect(() => {
+    if (type === "calendar") {
+      const fetchSchedules = async () => {
+        try {
+          const res = await api.get("/schedule/getList");
+
+          if (res.data.success && res.data.data) {
+            const apiOptions = res.data.data.map((item: any) => {
+              const today = dayjs().startOf("day");
+              const targetDate = dayjs(item.startSchedule).startOf("day");
+              const diffDays = targetDate.diff(today, "day");
+
+              let dDayString = "";
+              if (diffDays === 0) dDayString = "D-Day";
+              else if (diffDays > 0) dDayString = `D-${diffDays}`;
+              else dDayString = `D+${Math.abs(diffDays)}`;
+
+              return {
+                value: item.groupName,
+                label: `${item.scheduleFirstRegion} 여행`,
+                prefix: item.groupName,
+                suffix: dDayString,
+              };
+            });
+
+            setFetchedCalendarOptions([
+              { value: "default", label: "-", prefix: "내일정", suffix: "D-" },
+              ...apiOptions,
+            ]);
+          }
+        } catch (error) {
+          console.error("일정 목록을 불러오는데 실패했습니다.", error);
+        }
+      };
+
+      fetchSchedules();
+    }
+  }, [type]);
 
   const listOptions = useMemo<ScheduleOption[]>(
     () => [
@@ -55,14 +83,14 @@ const UseReactSelect = ({ type }: UseReactSelectProps) => {
       { value: "hotel", label: "숙소" },
       { value: "tourist_spot", label: "관광지" },
     ],
-    []
+    [],
   );
 
   const { options, customComponents, currentValue, onChangeAction } =
     useMemo(() => {
       if (type === "calendar") {
         return {
-          options: calendarOptions,
+          options: fetchedCalendarOptions,
           customComponents: {
             Option: CalendarOption,
             SingleValue: CalendarSingleValue,
@@ -85,24 +113,24 @@ const UseReactSelect = ({ type }: UseReactSelectProps) => {
       type,
       selectedCalendarSchedule,
       selectedListSchedule,
-      calendarOptions,
+      fetchedCalendarOptions,
       listOptions,
     ]);
 
   const handleChange = useCallback(
     (
       newValue: SingleValue<ScheduleOption>,
-      _meta: ActionMeta<ScheduleOption>
+      _meta: ActionMeta<ScheduleOption>,
     ) => {
       if (newValue) dispatch(onChangeAction(newValue));
     },
-    [dispatch, onChangeAction]
+    [dispatch, onChangeAction],
   );
 
   const filterOptions = useCallback(
     (opt: any) =>
       opt.data.value !== currentValue.value && opt.data.value !== "default",
-    [currentValue]
+    [currentValue],
   );
 
   return (
@@ -116,7 +144,6 @@ const UseReactSelect = ({ type }: UseReactSelectProps) => {
       value={currentValue}
       onChange={handleChange}
       components={customComponents}
-      defaultValue={options[0]}
       isSearchable={false}
       filterOption={filterOptions}
     />
