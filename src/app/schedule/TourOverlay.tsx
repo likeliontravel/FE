f6a7e32f9f4c90c0bfa6ec485cb8e9c6bcf22cd4
jwj9127 <jwj9127@naver.com>
2026-07-14 17:29:32 +0900

    style: 투어 가이드 이미지화 및 css 조정

--- src/app/schedule/TourOverlay.tsx
+++ src/app/schedule/TourOverlay.tsx
@@ -8,29 +8,21 @@ interface TourOverlayProps {
 
 export default function TourOverlay({ onClose }: TourOverlayProps) {
   return (
-    <div className={styles.overlay}>
-      {/* STEP 1 */}
-      <div className={`${styles.tooltip} ${styles.step1}`}>
-        <div className={styles.label}>STEP.1</div>
-        <div className={styles.text}>드롭다운에서 원하는 일정을 선택하세요</div>
-      </div>
-      {/* STEP 2 */}
-      <div className={`${styles.tooltip} ${styles.step2}`}>
-        <div className={styles.label}>STEP.2</div>
-        <div className={styles.text}>
-          맛집,숙소,관광지 중 카테고리를 선택하고 지역,테마(관광지 한정)를
-          선택하신 후 돋보기를 눌러주세요
-        </div>
-      </div>
-      {/* STEP 3 */}
-      <div className={`${styles.tooltip} ${styles.step3}`}>
-        <div className={styles.label}>STEP.3</div>
-        <div className={styles.text}>
-          일정박스를 열어 시간대 선택 후 조회된 데이터를 클릭해 추가해서 넣고
-          일정을 저장하세요
-        </div>
-      </div>
-      <button className={styles.closeBtn} onClick={onClose}>
+    <div className={styles.overlay} onClick={onClose}>
+      <img
+        src="/imgs/guide_overlay.png"
+        alt="서비스 이용 가이드"
+        className={styles.guideImg}
+        onClick={(e) => e.stopPropagation()}
+      />
+
+      <button
+        className={styles.closeBtn}
+        onClick={(e) => {
+          e.stopPropagation();
+          onClose();
+        }}
+      >
         확인
       </button>
     </div>
